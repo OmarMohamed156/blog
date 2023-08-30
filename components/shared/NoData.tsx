64dc9e577011244630a446a7b27087d7f9fb0897
OmarMohamed156 <om658036@gmail.com>
2023-08-31 01:49:28 +0300

    added nodata comp

---
+++ components/shared/NoData.tsx
@@ -0,0 +1,9 @@
+import { Flex } from '@mantine/core';
+import React from 'react';
+export default function NoData() {
+  return (
+    <Flex direction="row" w="100%" justify="center">
+      <h4>No Data Found 😢😢😢</h4>
+    </Flex>
+  );
+}
