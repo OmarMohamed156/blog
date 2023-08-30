b1c3e0c96bf355dca9334578a4b073532c521afb
OmarMohamed156 <om658036@gmail.com>
2023-08-31 02:53:44 +0300

    added types

--- typings/posts.d.ts
+++ typings/posts.d.ts
@@ -3,4 +3,43 @@ interface Post {
   userId: number;
   title: string;
   body: string;
+  user: User;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
 }
