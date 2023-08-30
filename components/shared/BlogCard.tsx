028562b3c3ea040e0998f6fe1675772e7266c67d
OmarMohamed156 <om658036@gmail.com>
2023-08-31 02:54:54 +0300

    added blog card

---
+++ components/shared/BlogCard.tsx
@@ -0,0 +1,62 @@
+import { createStyles, Paper, Text, Title, Button, rem } from '@mantine/core';
+import Link from 'next/link';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    minHeight: rem(350),
+    display: 'flex',
+    flexDirection: 'column',
+    // justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+
+  title: {
+    fontFamily: `Greycliff CF ${theme.fontFamily}`,
+    fontWeight: 900,
+    lineHeight: 1.2,
+    marginTop: theme.spacing.xs,
+  },
+
+  category: {
+    opacity: 0.7,
+    fontWeight: 700,
+    textTransform: 'uppercase',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  authorName: {
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    fontWeight: 700,
+    textTransform: 'uppercase',
+    fontSize: 12,
+  },
+}));
+
+interface ArticleCardImageProps {
+  post: Post;
+}
+
+export function BlogCard({ post }: ArticleCardImageProps) {
+  const { classes } = useStyles();
+
+  return (
+    <Link className={classes.link} href={`/posts/${post.id}`}>
+      <Paper shadow="md" p="xl" radius="md" className={classes.card}>
+        <div>
+          <Title order={5} className={classes.title}>
+            {post.title?.slice(0, 15)}
+          </Title>
+          <Text size="sm" mt={15} className={classes.authorName}>
+            {post.user?.name}
+          </Text>
+        </div>
+        <Text size="sm" mt={15} className={classes.category}>
+          {post.body?.slice(0, 100)}
+        </Text>
+      </Paper>
+    </Link>
+  );
+}
